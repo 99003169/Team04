c8cbdf576c12ba3b12b9a3b4c35706bb17300e9b
KaringulaSneha <75420644+KaringulaSneha@users.noreply.github.com>
2020-12-05 12:24:13 +0530

    Update function.c

--- MiniProject_C/src/function.c
+++ MiniProject_C/src/function.c
@@ -80,3 +80,22 @@ float power(float x, float y)
 {
     return (pow(x,y));
 }
+int add(int num1,int num2)
+{
+        return num1+num2;
+    }
+int subtract(int num1,int num2)
+{
+        return num1-num2;
+    }
+   int multiply(int num1,int num2)
+   {
+        return num1*num2;
+    }
+ float divide(int num1,int num2)
+    {
+        return (float)num1/(float)num2;
+    }
+    int mod(int num1,int num2){
+        return num1%num2;
+    }
