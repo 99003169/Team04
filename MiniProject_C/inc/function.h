742873b6e271b0f4591da28d105ec9509a35d047
megha.george@ltts.com <megha.george@ltts.com>
2020-12-05 10:55:44 +0530

    updated

---
+++ MiniProject_C/inc/function.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_H_INCLUDED
+#define FUNCTION_H_INCLUDED
+
+float do_celsius_fahrenheit(float);
+float do_fahrenheit_kelvin(float);
+float do_fahrenheit_celsius(float);
+float do_celsius_kelvin(float);
+float do_kelvin_celsius(float);
+float do_fahrenheit_kelvin(float);
