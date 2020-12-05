ca237255068ebf1869cc9cfacf54fd3e1a71a954
megha.george@ltts.com <megha.george@ltts.com>
2020-12-05 11:58:36 +0530

    update

--- MiniProject_C/inc/function.h
+++ MiniProject_C/inc/function.h
@@ -7,5 +7,15 @@ float do_fahrenheit_celsius(float);
 float do_celsius_kelvin(float);
 float do_kelvin_celsius(float);
 float do_fahrenheit_kelvin(float);
+float sine(float);
+float cosine(float);
+float tangent(float);
+float sineh(float);
+float cosineh(float);
+float tangenth(float);
+float logten(float);
+float squareroot(float);
+float exponent(float);
+float power(float, float);
 
 #endif
