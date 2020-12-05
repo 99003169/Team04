a27285fcfa7129d193ec09a5a0b2317f0e956062
megha.george@ltts.com <megha.george@ltts.com>
2020-12-05 11:07:01 +0530

    updated

--- MiniProject_C/main.c
+++ MiniProject_C/main.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-    float choice,input1,result;
+    float input1,result;
+    int choice;
     printf("\nPlease select the required operation to be performed \n");
     printf("1.celsius_fahrenheit\n");
     printf("2.fahrenheit_kelvin\n");
@@ -17,42 +18,42 @@ int main()
         case 1:
             printf("\nEnter temp to convert");
             scanf("%f",&input1);
-            result = do_celsius_fahrenheit(float input1);
+            result = do_celsius_fahrenheit(input1);
             printf("%f",result);
             break;
 
         case 2:
             printf("\nEnter temp to convert");
             scanf("%f",&input1);
-            result = do_fahrenheit_kelvin(float input1);
+            result = do_fahrenheit_kelvin(input1);
             printf("%f",result);
             break;
 
         case 3:
             printf("\nEnter temp to convert");
             scanf("%f",&input1);
-            result = do_fahrenheit_celsius(float input1);
+            result = do_fahrenheit_celsius(input1);
             printf("%f",result);
             break;
 
         case 4:
             printf("\nEnter temp to convert");
             scanf("%f",&input1);
-            result = do_celsius_kelvin(float input1);
+            result = do_celsius_kelvin(input1);
             printf("%f",result);
             break;
 
         case 5:
             printf("\nEnter temp to convert");
             scanf("%f",&input1);
-            result = do_kelvin_celsius(float input1)
+            result = do_kelvin_celsius(input1);
             printf("%f",result);
             break;
 
         case 6:
            printf("\nEnter temp to convert");
             scanf("%f",&input1);
-            result = do_fahrenheit_kelvin(float input1)
+            result = do_fahrenheit_kelvin(input1);
             printf("%f",result);
             break;
     }
