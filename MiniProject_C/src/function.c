8fb493efcb643e2c043002b93082d16da19a93e9
KaringulaSneha <75420644+KaringulaSneha@users.noreply.github.com>
2020-12-05 11:19:46 +0530

    Update function.c

--- MiniProject_C/src/function.c
+++ MiniProject_C/src/function.c
@@ -1,6 +1,7 @@
 #include<function.h>
 #include<stdio.h>
 #include<math.h>
+#define PI 3.14159265
 
 float do_celsius_fahrenheit(float input1)
 {
@@ -37,4 +38,45 @@ float do_fahrenheit_kelvin(float input1)
 {
     float result = 5/9(input1-32)+273;
     return result;
+}
+
+float sine(float x)
+{
+    return (sin (x*PI/180));
+}
+float cosine(float x)
+{
+    return (cos (x*PI/180));
+}
+float tangent(float x)
+{
+    return (tan(x*PI/180));
+}
+float sineh(float x)
+{
+    return (sinh(x));
+}
+float cosineh(float x)
+{
+    return (sinh(x));
+}
+float tangenth(float x)
+{
+    return (sinh(x));
+}
+float logten(float x)
+{
+    return (log10(x));
+}
+float squareroot(float x)
+{
+    return (sqrt(x));
+}
+float exponent(float x)
+{
+    return(exp(x));
+}
+float power(float x, float y)
+{
+    return (pow(x,y));
 }
