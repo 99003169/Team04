3c8582ebc8b965ab09656d71df882c47a2be980a
99003170 <75420372+99003170@users.noreply.github.com>
2020-12-05 15:47:45 +0530

    Update test_function.c

--- MiniProject_C/test/test_function.c
+++ MiniProject_C/test/test_function.c
@@ -15,7 +15,7 @@ void test_do_divide(void);
 void test_do_squareroot(void);
 void test_do_modulus(void);
 void test_do_factorial(void);
-//void test_do_exponent(void);
+void test_do_exponent(void);
 
 /* Start of the application test */
 int main() {
@@ -37,7 +37,7 @@ int main() {
   CU_add_test(suite, "TEST_SQUAREROOT", test_do_squareroot);
   CU_add_test(suite, "TEST_MODULUS", test_do_modulus);
   CU_add_test(suite, "TEST_FACTORIAL", test_do_factorial);
-  //CU_add_test(suite, "TEST_EXPONENT", test_do_exponent);
+  CU_add_test(suite, "TEST_EXPONENT", test_do_exponent);
 
 /* Note: Do not edit START*/
   /* Setup Test Framework to output the result to Screen */
@@ -51,26 +51,22 @@ int main() {
 /* Note: Do not edit END */
   return 0;
 }
-
-//---------------------------------TEMPERATURE CONVERSIONS----------------------------- 
-
+/* Write all the test functions */
 void test_do_celsius_fahrenheit(void) {
-  CU_ASSERT(86 == do_celsius_fahrenheit(30));
-  CU_ASSERT(68 == do_celsius_fahrenheit(20));
+  CU_ASSERT_FLOAT(86.0 == do_celsius_fahrenheit(30.0));
+  CU_ASSERT_FLOAT(68.0 == do_celsius_fahrenheit(20.0));
  
   /* Dummy fail*/
-  CU_ASSERT(100 == do_celsius_fahrenheit(25));
+  CU_ASSERT_FLOAT(100.0 == do_celsius_fahrenheit(25.0));
 }
 
 void test_do_fahrenheit_celsius(void) {
-  CU_ASSERT(37.7 == do_fahrenheit_celsius(100.0));
-  CU_ASSERT(32.2 == do_fahrenheit_celsius(90.0));
+  CU_ASSERT_FLOAT(37.7 == do_fahrenheit_celsius(100.0));
+  CU_ASSERT_FLOAT(32.2 == do_fahrenheit_celsius(90.0));
 
   /* Dummy fail*/
-  CU_ASSERT(2.3 == do_fahrenheit_celsius(80.0));
+  CU_ASSERT_FLOAT(2.3 == do_fahrenheit_celsius(80.0));
 }
-
-//--------------------------------------ARITHEMETIC OPERATIONS-----------------------------------
 
 void test_do_add(void) {
   CU_ASSERT_EQUAL(7, do_add(5,2));
@@ -79,44 +75,19 @@ void test_do_add(void) {
 }
 
 void test_do_subtract(void) {
-  CU_ASSERT_EQUAL(3, do_subtract(5,2));
-  CU_ASSERT_EQUAL(4, do_subtract(7,3));
-  CU_ASSERT_EQUAL(1000, do_subtract(1500,500));
+  CU_ASSERT_EQUAL(3, do_add(5,2));
+  CU_ASSERT_EQUAL(4, do_add(7,3));
+  CU_ASSERT_EQUAL(1000, do_add(1500,500));
 }
 
 void test_do_multiply(void) {
-  CU_ASSERT_EQUAL(10, do_multiply(5,2));
-  CU_ASSERT_EQUAL(21, do_multiply(7,3));
-  CU_ASSERT_EQUAL(5000, do_multiply(500,10));
+  CU_ASSERT_EQUAL(10, do_add(5,2));
+  CU_ASSERT_EQUAL(21, do_add(7,3));
+  CU_ASSERT_EQUAL(5000, do_add(500,10));
 }
 
 void test_do_divide(void) {
-  CU_ASSERT_EQUAL(5, test_do_divide(10,2));
-  CU_ASSERT_EQUAL(7, test_do_divide(7,1));
-  CU_ASSERT_EQUAL(3, test_do_divide(1500,500));
+  CU_ASSERT_EQUAL(5, do_add(10,2));
+  CU_ASSERT_EQUAL(7, do_add(7,1));
+  CU_ASSERT_EQUAL(3, do_add(1500,500));
 }
-
-void test_do_squareroot(void) {
-  CU_ASSERT_EQUAL(5, test_do_squareroot(25));
-  CU_ASSERT_EQUAL(10, test_do_squareroot(100));
-  CU_ASSERT_EQUAL(7, test_do_squareroot(49));
-}
-
-void test_do_modulus(void) {
-  CU_ASSERT_EQUAL(1, do_modulus(5,2));
-  CU_ASSERT_EQUAL(3, do_modulus(7,4));
-  CU_ASSERT_EQUAL(0, do_modulus(1500,500));
-}
-
-void test_do_factorial(void) {
-  CU_ASSERT_EQUAL(120, do_factorial(5));
-  CU_ASSERT_EQUAL(1, do_factorial(1));
-  CU_ASSERT_EQUAL(6, do_factorialdo_add(3));
-}
-
-/*
-void test_do_exponent(void) {
-  CU_ASSERT_EQUAL(5, do_exponent(1));
-  CU_ASSERT_EQUAL(7, do_exponent(7,1));
-  CU_ASSERT_EQUAL(3, do_exponent(1500,500));
-*/
